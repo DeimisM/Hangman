f5d3a0e6af958bd42601e3ddee22bda143d4887d
DeimisM <117855291+DeimisM@users.noreply.github.com>
2025-02-07 19:23:08 +0200

    Added while, if statements
    Checks, whether the letter is in the word or not

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import random
 
+
+is_word_guessed = False
+
 word_list = [
     "python", "giraffe", "keyboard", "puzzle", "mystery",
     "balloon", "adventure", "elephant", "champion", "umbrella",
@@ -8,12 +11,26 @@ word_list = [
             ]
 
 word = random.choice(word_list)
-length = len(word)
+word_letters = [*word]
 
+#print(word_letters)
 
+#word_length = len(word)
 
-print(word)
-print(length)
+while is_word_guessed != True:
+    users_guess = input("\n Try to guess the letters of the word:")
+
+    if users_guess.isalpha() & len(users_guess) == 1:
+        if users_guess in word_letters:
+            print("\t", users_guess, "is in the word!")
+
+        elif users_guess not in word_letters:
+            print("\t", users_guess, "is not in the word.")
+    else:
+        print("\t", "Your guess must only include 1 letter!")
+
+#print(word)
+#print(word_length)
 
 
 
