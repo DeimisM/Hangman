2464c64564a3de4f71e2fbf948cfc07de0dd5c1c
DeimisM <117855291+DeimisM@users.noreply.github.com>
2025-02-07 18:53:15 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import random
+
+word_list = [
+    "python", "giraffe", "keyboard", "puzzle", "mystery",
+    "balloon", "adventure", "elephant", "champion", "umbrella",
+    "treasure", "penguin", "dinosaur", "galaxy", "whistle",
+    "notebook", "volcano", "rainbow", "sapphire", "journey"
+            ]
+
+word = random.choice(word_list)
+length = len(word)
+
+
+
+print(word)
+print(length)
+
+
+
